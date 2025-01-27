70e026505338e6bb8065d4712c8b5220109362eb
Amit Kumar <amitk200415@gmail.com>
2025-01-28 01:26:31 +0530

    comment controller complete

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -4,16 +4,79 @@ import {ApiError} from "../utils/apiError.js"
 import {ApiResponse} from "../utils/apiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import { User } from "../models/user.model.js"
+import { Like } from "../models/like.model.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
 
+    if(!videoId){
+        throw new ApiError(404, "video id is required")        
+    }
+
+    const comments = await Comment.aggregate([
+        {
+            $match: new mongoose.Types.ObjectId(videoId)
+        },
+        {
+            $lookup: {
+                from: "users",
+                foreignField: "_id",
+                localField: "owner",
+                as: "owner",
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            fullname: 1,
+                            avatar: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $lookup: {
+                from: "likes",
+                foreignField: "comment",
+                localField: "_id",
+                as: "likedocuments"
+            }
+        },
+        {
+            $addFields: {
+                likes: {
+                    $size: "$likedocuments"
+                }
+            }
+        },
+        {
+            $project: {
+                content: 1,
+                likes: 1,
+                fullname: 1,
+                username: 1,
+                avatar: 1
+            }
+        },
+        {
+            $skip: (page -1) * limit
+        },
+        {
+            $limit: parseInt(limit)
+        }
+    ])
+
+    if(!comments.length){
+        throw new ApiError(404, "no comments found")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, comments, "comments fetched successfully"))
 })
 
 const addComment = asyncHandler(async (req, res) => {
-    // TODO: add a comment to a video
    
     const { content } = req.body;
     const { videoId } = req.params;
@@ -38,25 +101,29 @@ const addComment = asyncHandler(async (req, res) => {
 })
 
 const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
-
-    // take comment from req
-    // find comment which need to update from database
-    // change the comment
 
     const { content } = req.body
     const { commentId } = req.params
 
-    // const comment = Comment.findByIdAndUpdate(
-    //     commentId,
-    //     {
-    //         comment: content
-    //     }
-    // )
+    if(!content && !commentId){
+        throw new ApiError(404, "content and commentId are required")
+    }
 
-    // if(!comment){
-    //     throw new ApiError(500, "Something went wrong while updating comment")
-    // }
+    if(Comment.owner != req.user._id){
+        throw new ApiError(401, "Unauthorized request")
+    }
+
+    const updatedComment = await Comment.findByIdAndUpdate(
+        commentId,
+        {
+            $set: {content}
+        },
+        {new: true}
+    )
+
+    if(!updateComment){
+        throw new ApiError(500, "Something went wrong while updating comment")
+    }
 
     return res
     .status(200)
@@ -64,7 +131,29 @@ const updateComment = asyncHandler(async (req, res) => {
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
+
+    const {commentId} = req.params
+
+    if(!commentId){
+        throw new ApiError(404, "comment id is required")
+    }
+
+    if(Comment.owner != req.user._id){
+        throw new ApiError(401, "Unauthorized request")
+    }
+
+    const isDeleted = await Comment.findByIdAndDelete({commentId})
+
+    if(!isDeleted){
+        throw new ApiError(500, "Error while deleting comment")
+    }
+
+    await Like.deleteMany({comment: commentId})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "comment deleted successfully"))
+
 })
 
 export {
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -13,7 +13,6 @@ import {registerUser,
 } from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
-import { refreshAccessToken } from "../controllers/user.controller.js";
 
 const router = Router()
 
